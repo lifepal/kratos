1f11bb4e26b5b02d0b1b0d96beb83ea2d54057f9
Ferdina Kusumah <ferdinakusumah@Ferdinas-MacBook-Air.local>
2022-04-28 09:49:08 +0700

    remove local credential

--- selfservice/flow/login/helper.go
+++ selfservice/flow/login/helper.go
@@ -21,7 +21,8 @@ func getIssuer() string {
 func getFirebaseCredential() string {
 	var firebaseCred = os.Getenv("FIREBASE_CREDENTIAL_FILE")
 	if len(firebaseCred) == 0{
-		return "/Volumes/Dataku/Development/Go/go-firebase/django-auth-345906-firebase-adminsdk-jgqh7-85ea2d42c9.json"
+		// example: /home/django-auth-345906-firebase-adminsdk-jgqh7-85ea2d42c9.json
+		return ""
 	}
 	return firebaseCred
 }
