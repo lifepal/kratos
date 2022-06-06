6f9a64ab52713ae614f7a9dc5262472dc744aede
Ferdi <ferdina.kusumah@gmail.com>
2022-06-06 23:47:03 +0700

    add api GetOrganizationById

--- identity/handler.go
+++ identity/handler.go
@@ -126,6 +126,7 @@ func (h *Handler) RegisterAdminRoutes(admin *x.RouterAdmin) {
 	admin.PUT(ConfirmPasswordRoute, h.ConfirmPassword)
 	admin.PUT(ChangeUserInfoRoute, h.ChangeUserInfo)
 	admin.GET(GetUserWithOrganizationByIdRoute, h.GetUserWithOrganizationById)
+	admin.GET(GetOrganizationByIdRoute, h.GetOrganizationById)
 
 	admin.GET(RouteCollection, h.list)
 	admin.GET(RouteItem, h.get)
